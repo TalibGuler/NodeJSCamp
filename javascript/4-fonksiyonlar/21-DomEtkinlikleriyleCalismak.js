f11227cdd609516617b3dac7dd03fb473145c41f
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-01-22 16:21:07 +0300

    22-01-2022

---
+++ javascript/4-fonksiyonlar/21-DomEtkinlikleriyleCalismak.js
@@ -0,0 +1,9 @@
+//************** Dom Etkinlikleriyle Çalışmak **************/
+
+let greeting = document.querySelector("#greeting")
+greeting.addEventListener("click",domClick)
+
+function domClick() {
+   console.log("tıklandı")
+   this.style.color=="red" ? this.style.color="black":this.style.color="red"
+}
