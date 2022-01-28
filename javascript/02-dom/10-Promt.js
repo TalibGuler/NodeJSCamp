b08358b8ba818e917f2eb764d62401a7fef4b94d
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-01-28 21:27:56 +0300

    28-01-2022

--- javascript/02-dom/10-Promt.js
+++
@@ -1,7 +0,0 @@
-//************** Promt ile Kullanıcıdan Bilgi Almak **************/
-
-let fullName = prompt("Lütfen adınızı giriniz:")
-
-let greeting =document.querySelector("#greeting")
-
-greeting.innerHTML=`${greeting.innerHTML} <small style="color:red">${fullName}</small>`
--- javascript/04-fonksiyonlar/21-DomEtkinlikleriyleCalismak.js
+++
@@ -1,9 +0,0 @@
-//************** Dom Etkinlikleriyle Çalışmak **************/
-
-let greeting = document.querySelector("#greeting")
-greeting.addEventListener("click",domClick)
-
-function domClick() {
-   console.log("tıklandı")
-   this.style.color=="red" ? this.style.color="black":this.style.color="red"
-}
