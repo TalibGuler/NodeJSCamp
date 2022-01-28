e9cd92d167bd7901faa92b0921eff64db841cc23
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-01-28 20:12:47 +0300

    28-01-2022

--- javascript/06-form/26-FormSubmitYontemi.js
+++
@@ -1,9 +0,0 @@
-//************** Form Submit Yontemi **************/
-
-let formDOM = document.querySelector("#userForm")
-formDOM.addEventListener('submit',formSubmit)
-
-function formSubmit(event) {
-    event.preventDefault() // default islemi engelledik...
-    console.log("İşlem gerceklesti")
-}
