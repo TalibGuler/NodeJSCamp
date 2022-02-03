3c1234508dad1b04dc212197dcbb9c688746ec89
Abdülmuttalib GÜLER <64988496+TalibGuler@users.noreply.github.com>
2022-02-03 15:06:17 +0300

    03-02-2022

---
+++ node.js/3-nodejsTemelleri/odev3/circle.js
@@ -0,0 +1,21 @@
+const pi = 3.14;
+
+function circleArea(yariCap){
+    let alan = pi * (yariCap * yariCap)
+    console.log(`Alan: ${alan}`)
+}
+
+function circleCircumference(yariCap){
+    let cevre = 2 * pi * yariCap;
+    console.log(`Çevre: ${cevre}`)
+}
+
+// module.exports = circleArea;
+// module.exports = circleCircumference;
+
+module.exports = {
+    circleArea,
+    circleCircumference
+}
+
+// import
